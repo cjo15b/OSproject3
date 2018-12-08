b70ddab79251c552ead9c35cfadb193386c6252f
hodograph <jjdeanman@gmail.com>
2018-12-07 22:55:08 -0500

    added cmd args

--- system_main.c
+++ system_main.c
@@ -20,16 +20,20 @@ int main (int argc, const char* argv[]){		//this will be our main filesystem pro
    opened = (Status *) malloc(255 * sizeof(Status));
 
 	//IMPLEMENT AFTER FINISHING FOR CONVENIENCE
-	/*if (argc == 2){
-		if(fopen(argv[1]) != NULL)
+	if (argc == 2){
+      FILE * read = fopen(argv[1], "r");
+		if(read != NULL)
 		{
-			fclose(argv[1]);
+			fclose(read);
 		}
 		else
 		{
 			printf("Error: Invalid File Name");
 		}
-	}*/
+	}else{
+      printf("Arguments required\n");
+      return 1;
+   }
 	
 	// command prompt with hostname
 	/*
@@ -45,10 +49,10 @@ int main (int argc, const char* argv[]){		//this will be our main filesystem pro
 	*/
 	//To do: remove set fat32.img to argv[1]
 	//Setting the boot block and current dir to root
-	setBootBlock("fat32.img");
+   char* FAT32 = argv[1];
+	setBootBlock(FAT32);
 	initClusterNum();
 	while(1){
-
 
 		printf("Command-> ");	
 
@@ -70,30 +74,30 @@ int main (int argc, const char* argv[]){		//this will be our main filesystem pro
 			if(strcmp(command, "info") == 0)
 				info();
 			if(strcmp(command, "ls") == 0){
-	      		ls("fat32.img", arg); 
+	      		ls(FAT32, arg); 
 			}
 			if(strcmp(command, "size") == 0){
-	      		size("fat32.img", arg); 
+	      		size(FAT32, arg); 
 			}
          if(strcmp(command, "cd") == 0){
-	      	cd("fat32.img", arg); 
+	      	cd(FAT32, arg); 
 			}
 			if(strcmp(command, "mkdir") == 0){
-	      	mkdir("fat32.img", arg); 
+	      	mkdir(FAT32, arg); 
 			}
          if(strcmp(command, "creat") == 0){
-	      	creat("fat32.img", arg); 
+	      	creat(FAT32, arg); 
 			}
          if(strcmp(command, "open") == 0){
             token = strtok(NULL, " \n");
             if(token != NULL){
-               openMyFile("fat32.img", arg, token);
+               openMyFile(FAT32, arg, token);
             }else{
                printf("Need to specify a mode\n");
             }
          }
          if(strcmp(command, "close") == 0){
-            closeMyFile("fat32.img", arg);
+            closeMyFile(FAT32, arg);
          }
          if(strcmp(command, "read") == 0){
             char offset[256];
@@ -104,7 +108,7 @@ int main (int argc, const char* argv[]){		//this will be our main filesystem pro
                token = strtok(NULL, " \n");
                if(token != NULL){
                   strcpy(size, token);
-                  readMyFile("fat32.img", arg, offset, size);
+                  readMyFile(FAT32, arg, offset, size);
                }else{
                   printf("Not enough arguments\n");
                }
@@ -125,7 +129,7 @@ int main (int argc, const char* argv[]){		//this will be our main filesystem pro
                   token = strtok(NULL, "\n");
                   if(token != NULL){
                      strcpy(string, token);
-                     writeMyFile("fat32.img", arg, offset, size, string);
+                     writeMyFile(FAT32, arg, offset, size, string);
                   }else{
                      printf("Not enough arguments\n");
                   }
